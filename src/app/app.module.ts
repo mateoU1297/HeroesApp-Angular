222682a67d414b610a0d1b1988e9f2e05cfd8fa2
mateoU1297 <castro.mateo@correounivalle.edu.co>
2021-07-07 22:06:09 -0500

    se establece la conexión con la BD local, se crea el servicio de heroes y se implementa el modulo listado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
@@ -15,7 +16,8 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
